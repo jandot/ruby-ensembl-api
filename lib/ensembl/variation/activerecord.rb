86388a38d3f777c756e9d6372b4691efe22afa3c
Francesco Strozzi <francesco.strozzi@gmail.com>
2008-10-03 10:47:51 +0200

    created classes and associations according to Variation DB schema

--- lib/ensembl/variation/activerecord.rb
+++ lib/ensembl/variation/activerecord.rb
@@ -25,7 +25,9 @@ module Ensembl
     #  puts allele.to_yaml
     class Allele < DBConnection
       set_primary_key 'allele_id'
-      
+      belongs_to :sample
+      belongs_to :variation
+      belongs_to :population
     end
 
     # = DESCRIPTION
@@ -42,7 +44,149 @@ module Ensembl
     #  puts allele_group.to_yaml
     class AlleleGroup < DBConnection
       set_primary_key 'allele_group_id'
-      
+      belongs_to :variation_group
+      belongs_to :source
+      belongs_to :sample
+      belongs_to :allele_group_allele
     end
+    
+    class AlleleGroupAllele < DBConnection
+      belongs_to :variation
+      belongs_to :allele_group
+    end
+    
+    class Sample < DBConnection
+      set_primary_key "sample_id"
+      has_one :individual
+      has_one :sample_synonym
+      has_many :individual_genotype_multiple_bp
+      has_many :compressed_genotype_single_bp
+      has_many :read_coverage
+      has_one :population
+      has_many :tagged_variation_features
+    end
+    
+    class IndividualPopulation < DBConnection
+      belongs_to :individual
+      belongs_to :population
+    end
+    
+    class Individual < DBConnection
+      belongs_to :sample
+      #CAN'T FIGURE OUT SOME TABLE FIELDS
+    end
+    
+    class IndividualGenotypeMultipleBp < DBConnection
+      belongs_to :sample
+      belongs_to :variation
+    end
+    
+    class CompressedGenotypeSingleBp < DBConnection
+      belongs_to :sample
+    end
+    
+    class ReadCoverage < DBConnection
+      belongs_to :sample
+    end
+    
+    class Population < DBConnection
+      belongs_to :sample
+    end
+    
+    class PopulationStructure < DBConnection
+      #CAN'T FIGURE OUT SOME TABLE FIELDS
+    end
+    
+    class PopulationGenotype < DBConnection
+      set_primary_key "population_genotype_id"
+      belongs_to :variation
+      belongs_to :population
+    end
+    
+    class SampleSynonym < DBConnection
+      set_primary_key "sample_synonym_id"
+      belongs_to :source
+      belongs_to :sample
+      belongs_to :population
+    end
+    
+    class Source < DBConnection
+      set_primary_key "source_id"
+      has_many :sample_synonyms
+      has_many :allele_groups
+      has_many :variations
+      has_many :variation_groups
+      has_many :httags
+      has_many :variation_synonyms
+    end
+    
+    class VariationSynonym < DBConnection
+      set_primary_key "variation_synonym_id"
+      belongs_to :variation
+      belongs_to :source
+    end
+    
+    class Variation < DBConnection
+      set_primary_key "variation_id"
+      belongs_to :source
+      has_one :variation_synonym
+      has_one :flanking_sequence
+      has_many :allele_group_alleles
+      has_many :allele_groups, :through => :allele_group_alleles
+      has_many :population_genotypes
+      has_many :alleles
+      has_one :variation_feature
+      has_many :variation_group_variations
+      has_many :variation_groups, :through => :variation_group_variations
+      has_many :individual_genotype_multiple_bps
+    end
+    
+    class VariationGroup < DBConnection
+      set_primary_key "variation_group_id"
+      belongs_to :source
+      has_one :variation_group_variation
+      has_one :httag
+      has_one :variation_group_feature
+      has_one :allele_group
+    end
+    
+    class VariationGroupVariation < DBConnection
+      belongs_to :variation
+      belongs_to :variation_group
+    end
+    
+    class VariationFeature < DBConnection
+      set_primary_key "variation_feature_id"
+      belongs_to :variation
+      has_many :tagged_variation_features
+      has_many :samples, :through => :tagged_variation_features
+      has_many :transcript_variations
+    end
+    
+    class VariationGroupFeature < DBConnection
+      set_primary_key "variation_group_feature_id"
+      belongs_to :variation_group
+    end
+    
+    class TranscriptVariation < DBConnection
+      set_primary_key "transcript_variation_id"
+      belongs_to :variation_feature
+    end
+    
+    class FlankingSequence < DBConnection
+      belongs_to :variation
+    end
+    
+    class TaggedVariationFeature < DBConnection
+      belongs_to :variation_feature
+      belongs_to :sample
+    end
+    
+    class Httag < DBConnection
+      set_primary_key "httag_id"
+      belongs_to :variation_group
+      belongs_to :source
+    end
+    
   end
 end
