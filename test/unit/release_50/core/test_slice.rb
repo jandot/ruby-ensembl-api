519feb9816c84a75990a7ebeef6cf0eb9f564da7
Jan Aerts <jan.aerts@gmail.com>
2009-01-22 18:38:23 +0200

    Added test for excise and repeat_features

--- test/unit/release_50/core/test_slice.rb
+++ test/unit/release_50/core/test_slice.rb
@@ -60,12 +60,19 @@ class GetOverlappingObjects < Test::Unit::TestCase
     @small_slice = Slice.fetch_by_region('chromosome','Un.004.10515',850,900)
     @genes_inclusive = @small_slice.genes(true)
     @genes_exclusive = @small_slice.genes
+    
+    @large_slice = Slice.fetch_by_region('contig','AAFC03055312',1,18210)
+    @repeats_inclusive = @large_slice.repeat_features(true).select{|r| r.analysis_id == 6}
   end
   
   def test_get_gene
     assert_equal(1, @genes_inclusive.length)
     assert_equal('ENSBTAG00000039669', @genes_inclusive[0].stable_id)
     assert_equal(0, @genes_exclusive.length)
+  end
+  
+  def test_get_repeat_features
+    assert_equal(2, @repeats_inclusive.length)
   end
 end
 
@@ -98,6 +105,13 @@ class ExcisingSlice < Test::Unit::TestCase
 
   def test_excise_two_adjacent_ranges
     output = @original_slice.excise([20..99,100..200])
+    assert_equal(2, output.length)
+    assert_equal('chromosome:Btau_4.0:1:1:19:1', output[0].to_s)
+    assert_equal('chromosome:Btau_4.0:1:201:1000:1', output[1].to_s)
+  end
+  
+  def test_excise_internal_ranges
+    output = @original_slice.excise([20..300,100..200])
     assert_equal(2, output.length)
     assert_equal('chromosome:Btau_4.0:1:1:19:1', output[0].to_s)
     assert_equal('chromosome:Btau_4.0:1:201:1000:1', output[1].to_s)
