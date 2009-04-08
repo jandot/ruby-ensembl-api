f182e94c23405a03965ec0f97ff6565e9e79fe63
Francesco Strozzi <francesco.strozzi@gmail.com>
2009-04-08 12:20:58 +0200

    updated variation example at Ensembl release 53

--- samples/variation_example.rb
+++ samples/variation_example.rb
@@ -8,10 +8,10 @@ require File.dirname(__FILE__) + '/../lib/ensembl.rb'
 include Ensembl::Core
 include Ensembl::Variation
 
-Ensembl::Variation::DBConnection.connect('homo_sapiens',50)
+Ensembl::Variation::DBConnection.connect('homo_sapiens',53)
 # The connection with the Core database can be omitted. It is created automatically
 # when needed, using Variation DB connection parameters. The database name is derived 
-# from Variation DB name. If you are using non conventional DB names (i.e. local connection)
+# from Variation DB name. If you are using non conventional DB names (i.e. local Ensembl database)
 # an exception will be raised. Otherwise, if a Core DB connection is already 
 # present, that connection will be used by default, instead of creating a new one.
 
@@ -24,7 +24,7 @@ id.each do |i|
     up_seq,down_seq = vf.flanking_seq # retrieve upstream and downstream flanking sequences
     
     seq_region_name = vf.fetch_region.seq_region.name # fetch the genomic region of the Variation and get the region name.
-                                                      # Automatically sets the connection with Core DB, if necessary.
+                                                      # Automatically sets the connection with Core DB, if needed.
     
     puts "== VARIATION FEATURE =="
     print "NAME: #{vf.variation_name}\n ALLELE: #{vf.allele_string}\n UPSTREAM SEQ: #{up_seq.seq} \n DOWNSTREAM SEQ:  #{down_seq.seq}\n SEQ REGION NAME :#{seq_region_name}\n"
@@ -43,14 +43,14 @@ puts "== SEARCHING FOR VARIATIONS ON CHR:1:50000:51000 =="
 # Even in this case, Variation DB connection can be set automatically by specific Slice methods
 
 s = Slice.fetch_by_region('chromosome',1,50000,51000) 
-variation_features = s.get_variation_features # automatically sets the connection with Variation DB, if necessary.
+variation_features = s.get_variation_features # automatically sets the connection with Variation DB, if needed.
 variation_features.each do |vf|
   print "NAME: #{vf.variation_name}\n ALLELE: #{vf.allele_string}\n SEQ REGION NAME: #{vf.fetch_region.seq_region.name}\n"
 end
 
 puts "== GENOTYPED VARIATIONS =="
 
-genotyped_variation_features = s.get_genotyped_variation_features # automatically sets the connection with Variation DB, if necessary.
+genotyped_variation_features = s.get_genotyped_variation_features # automatically sets the connection with Variation DB, if needed.
 genotyped_variation_features.each do |gvf|
   print "NAME: #{gvf.variation_name} \n ALLELE: #{gvf.allele_string} \n SEQ REGION NAME: #{gvf.fetch_region.seq_region.name}\n"
 end
