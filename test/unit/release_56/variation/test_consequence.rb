9ab52c775bb156f9e6fc69e2ffa04cba156579f2
Francesco Strozzi <francesco.strozzi@gmail.com>
2010-11-17 16:43:37 +0100

    added more tests for several consequence types

--- test/unit/release_56/variation/test_consequence.rb
+++ test/unit/release_56/variation/test_consequence.rb
@@ -34,8 +34,55 @@ class TestVariation < Test::Unit::TestCase
   def test_3prime_utr
     vf = VariationFeature.new(:seq_region_id => 27506, :seq_region_start => 96810688, :seq_region_end => 96810688, :seq_region_strand => 1, :allele_string => "G/C", :variation_name => "rs16869283")
     tv = vf.transcript_variations
-    assert_equal("3PRIME_UTR", tv[0].consequence_type) 
+    assert_equal("3PRIME_UTR", tv[1].consequence_type) 
   end
   
+  def test_5prime_utr
+    vf = VariationFeature.new(:seq_region_id => 27506, :seq_region_start => 158536411, :seq_region_end => 158536411, :seq_region_strand => 1, :allele_string => "T/C", :variation_name => "rs71547565")
+    tv = vf.transcript_variations
+    assert_equal("5PRIME_UTR", tv[3].consequence_type) 
+  end
+  
+  def test_upstream
+    vf = VariationFeature.new(:seq_region_id => 27506, :seq_region_start => 96831018, :seq_region_end => 96831018, :seq_region_strand => 1, :allele_string => "G/T", :variation_name => "rs6975185")
+    tv = vf.transcript_variations
+    assert_equal("UPSTREAM",tv[0].consequence_type)
+  end
+  
+  def test_downstream
+    vf = VariationFeature.new(:seq_region_id => 27506, :seq_region_start => 105727321, :seq_region_end => 105727321, :seq_region_strand => 1, :allele_string => "G/T", :variation_name => "rs35113830")
+    tv = vf.transcript_variations
+    assert_equal("DOWNSTREAM",tv[0].consequence_type)
+  end
+  
+  def test_miRNA
+    vf = VariationFeature.new(:seq_region_id => 27518, :seq_region_start => 135895002, :seq_region_end => 135895002, :seq_region_strand => 1, :allele_string => "A/G", :variation_name => "rs11266800")
+    tv = vf.transcript_variations
+    assert_equal("WITHIN_MATURE_miRNA",tv[2].consequence_type) 
+  end
+  
+  def test_complex_indel_1
+    vf = VariationFeature.new(:seq_region_id => 27752, :seq_region_start => 37529, :seq_region_end => 37535, :seq_region_strand => 1, :allele_string => "CCACCCA/ACACCCG", :variation_name => "rs71228679")
+    tv = vf.transcript_variations
+    assert_equal("COMPLEX_INDEL",tv[0].consequence_type)  
+  end
+  
+  def test_essential_splice_site
+    vf = VariationFeature.new(:seq_region_id => 27511, :seq_region_start => 818059, :seq_region_end => 818059, :seq_region_strand => 1, :allele_string => "G/T", :variation_name => "rs3888067")
+    tv = vf.transcript_variations
+    assert_equal("ESSENTIAL_SPLICE_SITE",tv[0].consequence_type)  
+  end
+
+  def test_splice_site
+    vf = VariationFeature.new(:seq_region_id => 27506, :seq_region_start => 102301587, :seq_region_end => 102301587, :seq_region_strand => 1, :allele_string => "A/G", :variation_name => "rs434833")
+    tv = vf.transcript_variations
+    assert_equal("SPLICE_SITE",tv[5].consequence_type)  
+  end
+
+  def test_intronic
+    vf = VariationFeature.new(:seq_region_id => 27506, :seq_region_start => 101165365, :seq_region_end => 101165365, :seq_region_strand => 1, :allele_string => "T/C", :variation_name => "rs1859633")
+    tv = vf.transcript_variations
+    assert_equal("INTRONIC",tv[2].consequence_type)  
+  end
   
 end
