a8f9bce17edcbb36a01cef2998f551400a458082
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-05-02 17:49:19 +0200

    updated activerecord mappings for release 62

--- lib/ensembl/variation/activerecord.rb
+++ lib/ensembl/variation/activerecord.rb
@@ -69,6 +69,17 @@ module Ensembl
       set_primary_key "attrib_type_id"
     end 
     
+    # Store information on associated studies
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.    
+    class AssociateStudy < DBConnection
+      set_primary_key "study1_id"
+      belongs_to :study
+    end
+    
+    
     # This class uses ActiveRecord to access data in the Ensembl database.
     # See the general documentation of the Ensembl module for
     # more information on what this means and what methods are available.    
@@ -211,6 +222,41 @@ module Ensembl
       has_many :compressed_genotype_single_bps, :foreign_key => "sample_id"
     end
     
+    # The ProteinInfo class gives information about protein translated from a given transcript. 
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.
+    class ProteinInfo < DBConnection
+      set_primary_key "protein_info_id"
+      belongs_to :transcript_variation
+      has_many :protein_positions
+    end
+    
+    # The PolyphenPrediction class gives information about variations effect predictions within an aminoacidic sequence 
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.
+    class PolyphenPrediction < DBConnection
+      set_primary_key "polyphen_prediction_id"
+      belongs_to :protein_position
+    end  
+    
+    # The ProteinPosition class gives information about variations within an aminoacidic sequence. 
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.
+    class ProteinPosition < DBConnection
+      set_primary_key "protein_position_id"
+      belongs_to :protein_info
+      has_many :polyphen_predictions
+      has_many :sift_predictions
+    end
+    
+    
+    
     # The SampleSynonym class represents information about alternative names 
     # for sample entries. 
     #
@@ -240,6 +286,11 @@ module Ensembl
       has_many :variation_synonyms
       has_many :variation_annotations
       has_many :structural_variations
+      
+      def somatic_status # workaround as ActiveRecord do not parse SET field in MySQL
+        "#{attributes_before_type_cast['somatic_status']}" 
+      end
+      
     end
 
     # The StructuralVariation class gives information on structural variations mapped on the genome
@@ -251,6 +302,7 @@ module Ensembl
       set_primary_key "structural_variation_id"
       belongs_to :source
       belongs_to :seq_region
+      has_many :supporting_structural_variations
       
       class << self # Workaround for 'class' field, otherwise it creates a mess for AR
         def instance_method_already_implemented?(method_name)
@@ -272,6 +324,15 @@ module Ensembl
       has_many :structural_variations
     end
     
+    # The SubsnpHandle class gives information on SNP Submitters
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.    
+    class SupportingStructuralVariation < DBConnection
+      set_primary_key "supporting_structural_variation_id"
+      belongs_to :structural_variation
+    end
     
     # The SubsnpHandle class gives information on SNP Submitters
     #
@@ -284,6 +345,80 @@ module Ensembl
       has_many :population_genotypes, :foreign_key => "subsnp_id"
       has_many :alleles, :foreign_key => "subsnp_id"
       has_many :variation_synonyms,:foreign_key => "subsnp_id"
+    end
+    
+    # The SiftPrediction class gives information about variations effect predictions within an aminoacidic sequence
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.
+    class SiftPrediction < DBConnection
+      set_primary_key "sift_prediction_id"
+      belongs_to :protein_position
+    end
+    
+    # The Study class gives information about studies producing variations information
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.
+    class Study < DBConnection
+      set_primary_key "study_id"
+      has_many :associate_studies, :foreign_key => "study1_id"
+      has_many :structural_variations
+      has_many :variation_annotations
+      
+      def study_type
+        "#{attributes_before_type_cast['study_type']}"
+      end
+      
+    end
+
+
+    # The Variation class represents single nucleotide polymorhisms (SNP) or variations 
+    # and provides information like the names (IDs), the validation status and 
+    # the allele information.
+    #
+    # *BUG*: fields like validation_status and consequence_type are created 
+    # using SET option directly in MySQL. These fields are bad interpreted by
+    # ActiveRecord, returning always 0.
+    #
+    # This class uses ActiveRecord to access data in the Ensembl database.
+    # See the general documentation of the Ensembl module for
+    # more information on what this means and what methods are available.
+    #
+    # @example
+    #   v = Variation.find_by_name('rs10111')
+    #   v.alleles.each do |a|
+    #     puts a.allele, a.frequency
+    #   end
+    #
+    #   variations = Variation.fetch_all_by_source('dbSNP') # many records
+    #   variations.each do |v|
+    #     puts v.name
+    #   end
+    # 
+    class Variation < DBConnection
+      set_primary_key "variation_id"
+      belongs_to :source
+      has_many :variation_synonyms
+      has_one :flanking_sequence
+      has_many :allele_group_alleles
+      has_many :allele_groups, :through => :allele_group_alleles
+      has_many :population_genotypes
+      has_many :alleles
+      has_many :variation_features
+      has_many :variation_group_variations
+      has_many :variation_groups, :through => :variation_group_variations
+      has_many :individual_genotype_multiple_bps
+      has_many :failed_variations
+      has_many :failed_descriptions, :through => :failed_variations
+      has_many :variation_set_variations
+      has_many :variation_sets, :through => :variation_set_variations
+
+      def self.fetch_all_by_source(source)
+        variations = Source.find_by_name(source).variations
+      end
     end
     
     # The VariationSynonym class gives information on alterative names used 
